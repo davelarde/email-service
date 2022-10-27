9ce9d2b359a79984f00514490290b8154c249617
Preston Wallace <wallace.preston@gmail.com>
2022-10-27 12:33:13 -0700

    make email sending dynamic

--- handler.js
+++ handler.js
@@ -3,35 +3,48 @@ const AWS = require('aws-sdk');
 const ses = new AWS.SES({ region: 'us-west-2' });
 
 module.exports.sendEmail = async (event) => {
+  // select from the query parameters
+  const queryParams = event.queryStringParameters || {};
+  let { email, message, subject } = queryParams;
+
+  // if no email is provided, return an error
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Email is required',
+      }),
+    };
+  }
+  message = message || 'This is a message generated automatically from a Lambda function.';
+  subject = subject || 'Hello from Lambda';
+
   const params = {
     Destination: {
-      ToAddresses: ['wallace.preston@gmail.com'], // This should be your email address
+      ToAddresses: [email],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'This is a message generated automatically from a Lambda function.',
+          Data: message,
         },
       },
       Subject: {
-        Data: 'Hello from Lambda',
+        Data: subject,
       },
     },
-    Source: 'wallace.preston@gmail.com', // This can be any email address, the email you want to show as the "sender" when the email is received
+    Source: 'wallace.preston@gmail.com', // The email you want to show as the "sender" when the email is received.  This must be added as an identity via the AWS SES console and verified.
   };
   await ses.sendEmail(params).promise();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
